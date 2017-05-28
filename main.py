e46201c76c2de84fb82adeb168a4d51c9a0d10f5
tsh <dr.tallin@gmail.com>
2017-05-28 20:11:59 +0300

    split to classes

--- main.py
+++ main.py
@@ -9,21 +9,26 @@ from  kivy.uix.image import Image
 from kivy.vector import Vector
 
 
-class PongBall(Widget):
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
+class Tile(Widget):
+    def __init__(self):
+        super().__init__()
+        self.resource = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'resources', 'block_1.png')
 
     def draw(self):
-        self.pos = Vector(*self.velocity) + self.pos
+        with self.canvas:
+            Rectangle(source=self.resource, pos=(40, 22))
+
+
+class Character(Widget):
+    def draw(self):
+        self.pos = (20,10)
         with self.canvas:
             Color(1, 1, 0)
-            Ellipse(pos=self.pos, size=(70,20))
-            Ellipse(pos=[200, 200], size=(70,20))
-            Rectangle(source=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'resources', 'block_1.png'), pos=(40,22))
+            Ellipse(pos=self.pos, size=(70, 20))
+            Ellipse(pos=[200, 200], size=(70, 20))
 
 
-class PongGame(Widget):
+class Game(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
@@ -32,12 +37,13 @@ class PongGame(Widget):
             children.draw()
 
 
-
 class MyApp(App):
     def build(self):
-        ball = PongBall()
-        game = PongGame()
-        game.add_widget(ball)
+        tile = Tile()
+        character = Character()
+        game = Game()
+        game.add_widget(tile)
+        game.add_widget(character)
         Clock.schedule_interval(game.update, 1.0/60.0)
         return game
 
