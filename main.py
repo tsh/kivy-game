4fa19e4b722c6fa4c960e5537addb1534f3838ed
tsh <dr.tallin@gmail.com>
2017-05-28 20:27:43 +0300

    draw map

--- main.py
+++ main.py
@@ -10,20 +10,21 @@ from kivy.vector import Vector
 
 
 class Tile(Widget):
-    def __init__(self):
+    def __init__(self, x=0, y=0):
         super().__init__()
         self.resource = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'resources', 'block_1.png')
+        self.x = x
+        self.y = y
 
     def draw(self):
         with self.canvas:
-            Rectangle(source=self.resource, pos=(40, 22))
+            Rectangle(source=self.resource, pos=(self.x, self.y))
 
 
 class Character(Widget):
     def draw(self):
         self.pos = (20,10)
         with self.canvas:
-            Color(1, 1, 0)
             Ellipse(pos=self.pos, size=(70, 20))
             Ellipse(pos=[200, 200], size=(70, 20))
 
@@ -39,13 +40,24 @@ class Game(Widget):
 
 class MyApp(App):
     def build(self):
-        tile = Tile()
         character = Character()
         game = Game()
-        game.add_widget(tile)
         game.add_widget(character)
+        self.draw_background(game)
         Clock.schedule_interval(game.update, 1.0/60.0)
         return game
+
+    def draw_background(self, game):
+        TILE_SIZE = 100
+
+        map = [[1,0,1],
+               [0,1,0]]
+
+        # `reverse` to draw map on canvas as it seen in code.
+        for y, row in enumerate(reversed(map)):
+            for x, tile in enumerate(row):
+                if tile == 1:
+                    game.add_widget(Tile(x=x*TILE_SIZE, y=y*TILE_SIZE))
 
 
 if __name__ == '__main__':
